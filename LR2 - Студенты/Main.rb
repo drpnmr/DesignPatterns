b66269dd1af73e64f25d9911d9ef61b1b122d12d
Darya <darya.pnmr@gmail.com>
2024-10-18 20:11:45 +0300

    ЛР2, задание 1.9. Тестирования метода валидации (тут все ок)

--- LR2 - Студенты/Main.rb
+++ LR2 - Студенты/Main.rb
@@ -1,6 +1,6 @@
 require './Student.rb'
 
-st1 = Student.new(email:"darya.pnmr@gmail.com", surname:"Пономарь", name:"Дарья", patronymic:"Сергеевна", id: 1, phone:"89281234567", git: "drpnmr1", telegram:"drpnmr")
+st1 = Student.new(email:"darya.pnmr@gmail.com", surname:"Пономарь", name:"Дарья", patronymic:"Сергеевна", id: 1, phone:"+79281234567", telegram:"drpnmr", git:"drpnmr")
 
+st1.validate()
 st1.show_info()
-
