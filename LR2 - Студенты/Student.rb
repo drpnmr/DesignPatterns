10aa86286af7ae30cf2a58f146337d18f915c4eb
Darya <darya.pnmr@gmail.com>
2024-10-18 19:00:43 +0300

    ЛР2, задание 1.8. Валидация остальных полей (телеграма, почты, гита)

--- LR2 - Студенты/Student.rb
+++ LR2 - Студенты/Student.rb
@@ -7,9 +7,9 @@ class Student
     @patronymic = args[:patronymic] || raise(ArgumentError, "Отчество обязательно")
     @id = args[:id]
     self.phone = args[:phone] 
-    @telegram = args[:telegram]
-    @email = args[:email]
-    @git = args[:git]
+    self.telegram = args[:telegram]
+    self.email = args[:email]
+    self.git = args[:git]
   end
 
   def self.phone_regular(phone)
@@ -19,6 +19,33 @@ class Student
   def phone=(phone)
     raise ArgumentError, "Недопустимый номер телефона" unless self.class.phone_regular(phone)
     @phone = phone
+  end
+
+  def self.telegram_regular(telegram)
+    telegram =~ /^[a-zA-Z0-9_-]{1,20}$/
+  end
+
+  def telegram=(telegram)
+    raise ArgumentError, "Недопустимый телеграм" unless self.class.telegram_regular(telegram)
+    @telegram = telegram
+  end
+
+  def self.email_regular(email)
+    email =~ /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  end
+
+  def email=(email)
+    raise ArgumentError, "Недопустимый адрес почты" unless self.class.email_regular(email)
+    @email = email
+  end
+
+  def self.git_regular(git)
+    git =~ /^[a-zA-Z0-9_-]{1,20}$/
+  end
+
+  def git=(git)
+    raise ArgumentError, "Недопустимый гит" unless self.class.git_regular(git)
+    @git = git
   end
 
   def show_info()
