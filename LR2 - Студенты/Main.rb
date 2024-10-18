0e8de547481ff07e652e20bc6adad6eb08934545
Darya <darya.pnmr@gmail.com>
2024-10-18 16:03:22 +0300

    ЛР2, задание 1.3. Добавление нового студента для тестирования show_info

--- LR2 - Студенты/Main.rb
+++ LR2 - Студенты/Main.rb
@@ -3,7 +3,10 @@ require './Student.rb'
 st1 = Student.new("Пономарь", "Дарья", "Сергеевна", 1, "89281234567", "drpnmr", "darya.pnmr@gmail.com", "drpnmr")
 st2 = Student.new("Иванов", "Иван", "Иванович", 2, "89181234567", "ivanov_ivan", "ivanov@mail.ru", "iiivanov")
 st3 = Student.new("Петров", "Петр", "Петрович", 3, "89601234567", "hiphd_0", "petrov2098@mail.ru", "hiphd")
+st4 = Student.new("Кузнецов", "Николай", "Владимирович")
+
 
 st1.show_info()
 st2.show_info()
 st3.show_info()
+st4.show_info()
