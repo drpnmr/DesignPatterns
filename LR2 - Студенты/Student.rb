fcd0d5617d43b020c117ccb2501fb9c84c8a8340
Darya <darya.pnmr@gmail.com>
2024-10-18 16:01:37 +0300

    ЛР2, задание 1.3. Геттеры и сеттеры заменили на attr_accessor, вывод всех полей для show_info, даже если пустые.

--- LR2 - Студенты/Student.rb
+++ LR2 - Студенты/Student.rb
@@ -1,68 +1,31 @@
 class Student
 
+  attr_accessor :surname, :name, :patronymic, :id, :phone, :telegram, :email, :git
+  
   def initialize(surname, name, patronymic, id = nil, phone = nil, telegram = nil, email = nil, git = nil)
-      @surname = surname
-      @name = name
-      @patronymic = patronymic
-	    @id = id
-      @phone = phone
-      @telegram = telegram
-      @email = email
-      @git = git
+    @surname = surname
+    @name = name
+    @patronymic = patronymic
+	  @id = id
+    @phone = phone
+    @telegram = telegram
+    @email = email
+    @git = git
   end
 
-  
-  def get_surname; @surname; end
-  def set_surname(value)
-    @surname = value
-  end
-  
-  def get_name; @name; end
-  def set_name(value)
-    @name = value
-  end
-  
-  def get_patronymic; @patronymic; end
-  def set_patronymic(value)
-    @patronymic = value
-  end
-  
-  def get_id; @id; end
-  def set_id(value)
-    @id = value
-  end
-
-  def get_phone; @phone; end
-  def set_phone(value)
-    @phone = value
-  end
-
-  def get_telegram; @telegram; end
-  def set_telegram(value)
-    @telegram = value
-  end
-
-  def get_email; @email; end
-  def set_email(value)
-    @email = value
-  end
-  
-  def get_git; @git; end
-  def set_git(value)
-    @git = value
-  end
   
   def show_info()
-    puts "Id студента: #{@id}" if @id
+    puts "Id студента: #{@id || 'не указано'}"
     puts "Фамилия: #{@surname}"
     puts "Имя: #{@name}"
     puts "Отчество: #{@patronymic}"
-    puts "Телефон: #{@phone}" if @phone
-    puts "Телеграм: #{@telegram}" if @telegram
-    puts "Почта: #{@email}" if @email
-    puts "Гит: #{@git}" if @git
+    puts "Телефон: #{@phone || 'не указано'}"
+    puts "Телеграм: #{@telegram || 'не указано'}"
+    puts "Почта: #{@email || 'не указано'}"
+    puts "Гит: #{@git || 'не указано'}"
     puts ""
   end
+
 end
 
 
