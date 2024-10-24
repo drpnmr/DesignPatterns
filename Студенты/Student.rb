d1002b3986469f3c40da69d7e59c49e8cc72b5e0
Darya <darya.pnmr@gmail.com>
2024-10-24 22:50:49 +0300

    Исправила замечания

--- Студенты/Student.rb
+++ Студенты/Student.rb
@@ -1,101 +1,119 @@
 class Student
-  attr_accessor :surname, :name, :patronymic, :id, :phone, :telegram, :email, :git
-  
-  def initialize(args = {})
-    @surname = args[:surname] || raise(ArgumentError, "Фамилия обязательна")
-    @name = args[:name] || raise(ArgumentError, "Имя обязательно")
-    @patronymic = args[:patronymic] || raise(ArgumentError, "Отчество обязательно")
-    @id = args[:id]
-    self.set_contacts(args)
-    self.git = args[:git]
-  end
+  attr_accessor :id
+  attr_reader :phone, :name, :surname, :patronymic, :telegram, :email, :git
+ 
+ def initialize(args = {})
+   self.surname = args[:surname] || raise(ArgumentError, "Фамилия обязательна")
+   self.name = args[:name] || raise(ArgumentError, "Имя обязательно")
+   self.patronymic = args[:patronymic] || raise(ArgumentError, "Отчество обязательно")
+   @id = args[:id]
+   self.set_contacts(args)
+   self.git = args[:git]
+ end
 
-  def set_contacts(contacts)
-    self.phone = contacts[:phone]
-    self.telegram = contacts[:telegram]
-    self.email = contacts[:email]
-  end
+ def set_contacts(contacts)
+   self.phone = contacts[:phone]
+   self.telegram = contacts[:telegram]
+   self.email = contacts[:email]
+ end
 
-  def self.phone_regular(phone)
-    phone =~ /^\+7\d{10}$/
-  end
+ def self.is_correct_name?(name)
+   name =~ /^[А-Яа-яЁё\s]+$/ 
+ end
 
-  private def phone=(phone)
-    if phone.nil? || phone.empty?
-      @phone = nil
-    else
-      raise ArgumentError, "Недопустимый номер телефона у студента с id: #{self.id}" unless self.class.phone_regular(phone)
-      @phone = phone
-    end
-  end
+ private def surname=(surname)
+   raise ArgumentError, "Недопустимая фамилия у студента с id: #{self.id}" unless self.class.is_correct_name?(surname)
+   @surname = surname
+ end
 
-  def self.telegram_regular(telegram)
-    telegram =~ /^[a-zA-Z0-9_-]{4,20}$/
-  end
+ private def name=(name)
+   raise ArgumentError, "Недопустимое имя у студента с id: #{self.id}" unless self.class.is_correct_name?(name)
+   @name = name
+ end
 
-  private def telegram=(telegram)
-    if telegram.nil? || telegram.empty?
-      @telegram = nil
-    else
-      raise ArgumentError, "Недопустимый телеграм у студента с id: #{self.id}" unless self.class.telegram_regular(telegram)
-      @telegram = telegram
-    end
-  end
+ private def patronymic=(patronymic)
+   raise ArgumentError, "Недопустимое отчество у студента с id: #{self.id}" unless self.class.is_correct_name?(patronymic)
+   @patronymic = patronymic
+ end
 
-  def self.email_regular(email)
-    email =~ /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  end
+ def self.is_correct_phone?(phone)
+   phone =~ /^\+7\d{10}$/
+ end
 
-  private def email=(email)
-    if email.nil? || email.empty?
-      @email = nil
-    else
-      raise ArgumentError, "Недопустимый адрес почты у студента с id: #{self.id}" unless self.class.email_regular(email)
-      @email = email
-    end
-  end
+ private def phone=(phone)
+   if phone.nil? || phone.empty?
+     @phone = nil
+   else
+     raise ArgumentError, "Недопустимый номер телефона у студента с id: #{self.id}" unless self.class.is_correct_phone?(phone)
+     @phone = phone
+   end
+ end
 
-  def self.git_regular(git)
-    git =~ /^[a-zA-Z0-9_-]{4,20}$/
-  end
+ def self.is_correct_telegram?(telegram)
+   telegram =~ /^[a-zA-Z0-9_-]{4,20}$/
+ end
 
-  def git=(git)
-    if git.nil? || git.empty?
-      @git = nil
-    else
-      raise ArgumentError, "Недопустимый гит у студента с id: #{self.id}" unless self.class.git_regular(git)
-      @git = git
-    end
-  end
+ private def telegram=(telegram)
+   if telegram.nil? || telegram.empty?
+     @telegram = nil
+   else
+     raise ArgumentError, "Недопустимый телеграм у студента с id: #{self.id}" unless self.class.is_correct_telegram?(telegram)
+     @telegram = telegram
+   end
+ end
 
-  def check_git?
-    @git.nil? 
-  end
+ def self.is_correct_email?(email)
+   email =~ /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+ end
 
-  def check_contact?
-    !@phone.nil? || !@telegram.nil? || !@email.nil?
-  end
+ private def email=(email)
+   if email.nil? || email.empty?
+     @email = nil
+   else
+     raise ArgumentError, "Недопустимый адрес почты у студента с id: #{self.id}" unless self.class.is_correct_email?(email)
+     @email = email
+   end
+ end
 
-  def validate
-    if check_git?
-      raise ArgumentError, "У студента с id '#{self.id}' отсутствует гит"
-    end
-    unless check_contact?
-      raise ArgumentError, "У студента с id '#{self.id}' нет ни одного контакта"
-    end
-  end
+ def self.is_correct_git?(git)
+   git =~ /^[a-zA-Z0-9_-]{4,20}$/
+ end
 
-  def show_info()
-    puts "Id студента: #{@id || 'не указано'}"
-    puts "Фамилия: #{@surname}"
-    puts "Имя: #{@name}"
-    puts "Отчество: #{@patronymic}"
-    puts "Телефон: #{@phone || 'не указано'}"
-    puts "Телеграм: #{@telegram || 'не указано'}"
-    puts "Почта: #{@email || 'не указано'}"
-    puts "Гит: #{@git || 'не указано'}"
-    puts ""
-  end
+ def git=(git)
+   if git.nil? || git.empty?
+     @git = nil
+   else
+     raise ArgumentError, "Недопустимый гит у студента с id: #{self.id}" unless self.class.is_correct_git?(git)
+     @git = git
+   end
+ end
 
+ def check_git?
+   @git.nil? 
+ end
+
+ def check_contact?
+   !@phone.nil? || !@telegram.nil? || !@email.nil?
+ end
+
+ def validate
+   if check_git?
+     "У студента с id '#{self.id}' отсутствует гит"
+   end
+   unless check_contact?
+     "У студента с id '#{self.id}' нет ни одного контакта"
+   end
+ end
+
+ def to_s()
+  "  Фамилия: #{surname}
+  Имя: #{name}
+  Отчество: #{patronymic}
+  Id студента: #{@id ? @id : "не указано"}
+  Телефон: #{@phone ? @phone : "не указано"}
+  Телеграм: #{@telegram ? @telegram : "не указано"}
+  Почта: #{@email ? @email : "не указано"}
+  Гит: #{@git ? @git : "не указано"}\n"
+ end
 
 end
