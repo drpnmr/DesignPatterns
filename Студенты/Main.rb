978ee86640cfb293223eb96bf80c4bf42a00c00e
Darya <darya.pnmr@gmail.com>
2024-10-24 22:51:31 +0300

    Тест to_s

--- Студенты/Main.rb
+++ Студенты/Main.rb
@@ -3,4 +3,4 @@ require './Student.rb'
 st1 = Student.new(email:"darya.pnmr@gmail.com", surname:"Пономарь", name:"Дарья", patronymic:"Сергеевна", id: 1, phone:"+79281234567", telegram:"drpnmr", git:"drpnmr")
 
 st1.validate()
-st1.show_info()
+puts st1
