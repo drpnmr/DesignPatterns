2c788d71314a231e929f3fe10276fd8e04038182
Darya <darya.pnmr@gmail.com>
2024-10-25 21:18:09 +0300

    ЛР2, задание 2.3. Реализация get_info

--- Студенты/Main.rb
+++ Студенты/Main.rb
@@ -2,5 +2,5 @@ require './Student.rb'
 
 st1 = Student.new(email:"darya.pnmr@gmail.com", surname:"Пономарь", name:"Дарья", patronymic:"Сергеевна", id: 1, phone:"+79281234567", telegram:"drpnmr", git:"drpnmr")
 
-st1.validate()
-puts st1
+puts st1.get_info
+
