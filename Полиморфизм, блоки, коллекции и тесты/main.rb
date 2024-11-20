fd794e7697ac8eb19ce5dbb333ea3b50d8a812d7
Darya <darya.pnmr@gmail.com>
2024-11-20 15:57:25 +0300

    ЛР3, задание 1.36. Использование метода создания списка индексов элемента с наибольшей частотой вхождения

--- Полиморфизм, блоки, коллекции и тесты/main.rb
+++ Полиморфизм, блоки, коллекции и тесты/main.rb
@@ -10,8 +10,12 @@ array = [2, 1, 2, 3, 4, 5, 8]
 #largest, second_largest = find_two_largest_elements(array)
 #puts "Наибольшие элементы: #{largest}, #{second_largest}"
 
-result = max_odd_element(array)
-puts "Максимальный нечётный элемент: #{result}" 
+#result = max_odd_element(array)
+#puts "Максимальный нечётный элемент: #{result}" 
+
+result = indices_of_most_frequent_elements(array)
+puts "Индексы элементов, которые повторяются наибольшее количество раз: #{result}"
+  
   
 
 
