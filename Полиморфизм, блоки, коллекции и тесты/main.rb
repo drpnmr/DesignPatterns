d431becf4ae753b255d8468a8db592d8906c31b0
Darya <darya.pnmr@gmail.com>
2024-11-20 16:19:03 +0300

    ЛР3, задание 1.60. Использование метода поиска элементов, делящихся на номер и уникальных

--- Полиморфизм, блоки, коллекции и тесты/main.rb
+++ Полиморфизм, блоки, коллекции и тесты/main.rb
@@ -1,6 +1,6 @@
 require './no_loop_methods.rb'
 
-array = [2, 1, 2, 3, 4, 5, 8]
+#array = [2, 1, 2, 3, 4, 5, 8]
 
 #result = reverse_between_min_max(array)
 #puts "Исходный массив: #{array}"
@@ -13,10 +13,13 @@ array = [2, 1, 2, 3, 4, 5, 8]
 #result = max_odd_element(array)
 #puts "Максимальный нечётный элемент: #{result}" 
 
-result = indices_of_most_frequent_elements(array)
-puts "Индексы элементов, которые повторяются наибольшее количество раз: #{result}"
-  
-  
+#result = indices_of_most_frequent_elements(array)
+#puts "Индексы элементов, которые повторяются наибольшее количество раз: #{result}"
+
+array = [4, 2, 18, 8, 5, 12, 7, 14, 18]
+result = elements_divisible_by_index_and_unique(array)
+puts "Результат: #{result}" 
+
 
 
   
