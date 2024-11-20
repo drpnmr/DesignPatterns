c6370825ce69e26d85660ace704a37c55a0abd77
Darya <darya.pnmr@gmail.com>
2024-11-20 15:33:00 +0300

    ЛР3, задание 1.36. Использование метода поиска максимального нечетного элемента

--- Полиморфизм, блоки, коллекции и тесты/main.rb
+++ Полиморфизм, блоки, коллекции и тесты/main.rb
@@ -1,15 +1,18 @@
 require './no_loop_methods.rb'
 
-array = [2, 1, 2, 3, 4, 5]
+array = [2, 1, 2, 3, 4, 5, 8]
 
 #result = reverse_between_min_max(array)
 #puts "Исходный массив: #{array}"
 #puts "Результат: #{result}"
 
 
-largest, second_largest = find_two_largest_elements(array)
-puts "Наибольшие элементы: #{largest}, #{second_largest}"
+#largest, second_largest = find_two_largest_elements(array)
+#puts "Наибольшие элементы: #{largest}, #{second_largest}"
 
+result = max_odd_element(array)
+puts "Максимальный нечётный элемент: #{result}" 
+  
 
 
   
