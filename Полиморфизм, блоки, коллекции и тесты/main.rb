d86682afa1da8c9bc8bdb0825790c6f86623841b
Darya <darya.pnmr@gmail.com>
2024-11-20 14:37:30 +0300

    ЛР3, задание 1.12. Использование метода поиска двух наибольших элементов массива

--- Полиморфизм, блоки, коллекции и тесты/main.rb
+++ Полиморфизм, блоки, коллекции и тесты/main.rb
@@ -1,6 +1,15 @@
 require './no_loop_methods.rb'
 
 array = [2, 1, 2, 3, 4, 5]
-result = reverse_between_min_max(array)
-puts "Исходный массив: #{array}"
-puts "Результат: #{result}"
+
+#result = reverse_between_min_max(array)
+#puts "Исходный массив: #{array}"
+#puts "Результат: #{result}"
+
+
+largest, second_largest = find_two_largest_elements(array)
+puts "Наибольшие элементы: #{largest}, #{second_largest}"
+
+
+
+  
