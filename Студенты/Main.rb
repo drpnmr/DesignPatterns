e2f3aacde5476cf643d06175183a4ec3e3ad1362
Darya <darya.pnmr@gmail.com>
2024-12-19 13:38:52 +0300

    ЛР3, задание 4. Создание дерева чисел, дерева студентов, использование select

--- Студенты/Main.rb
+++ Студенты/Main.rb
@@ -6,15 +6,20 @@ st1 = Student.new(email:"darya.pnmr@gmail.com", surname:"Пономарь", name
 student1 = Student_short.from_string(id: 1, string: "Пономарь Д.С., darya.pnmr@gmail.com, drpnmr")
 
 st2 = Student.new(id: 2, surname: "Иванов", name: "Иван", patronymic: "Иванович", birth_date: "2000-01-15", git: "ivan_git", telegram:"ivan123")
-st3 = Student.new(id: 3, surname: "Сидоров", name: "Сидор", patronymic: "Сидорович", birth_date: "2001-07-20", git: "sidor_git")
+st3 = Student.new(id: 3, surname: "Сидоров", name: "Сидор", patronymic: "Сидорович", birth_date: "2000-03-30", git: "sidor_git")
+
+tree_num = StudentTree.new
+tree_num.add(1)
+tree_num.add(5)
+tree_num.add(2)
+
+tree_num.each do |node|
+    puts node
+end
 
 tree = StudentTree.new
 tree.add(st1)
 tree.add(st2)
 tree.add(st3)
-
-tree.each do |student|
-    puts student
-end
 
 puts tree.select {|student| student.surname=="Пономарь"}
