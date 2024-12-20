37e605832ebe320dff9283c0a4caae2e1c01da34
Darya <darya.pnmr@gmail.com>
2024-12-20 10:50:37 +0300

    ЛР4, задание 1.4. Реализация методов выделения элементов по номеру и получения массива таких элементов

--- Студенты/Data_list.rb
+++ Студенты/Data_list.rb
@@ -1,12 +1,34 @@
 class Data_list
 
   def initialize(list)
-    self.list = list
+    self.list = list.sort()
+    self.selected = []
   end
   
+  def select(number)
+    if number >= 0 && number < list.size
+      selected << number unless selected.include?(number)
+    else
+      raise IndexError, "Недопустимый индекс"
+    end
+  end
+
+  def get_selected
+    selected.map { |index| list[index] }
+  end
+
+  def get_names
+
+  end
+
+  def get_data
+
+  end
+
   private
 
   attr_reader :list
+  attr_accessor :selected
   
   def list=(list)
     if list.is_a?(Array)
@@ -15,4 +37,6 @@ class Data_list
         raise TypeError, "Поле должно являться массивом"
     end
   end
+  
 end
+
